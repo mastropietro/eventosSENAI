e21bfa2248d8f7362079957da7a4356efe4df1ab
NICHOLLAS MASTROPIETRO RODRIGUES DOS SANTOS <26175318@educ123.sp.senai.br>
2026-09-22 14:51:05 -0300

    FEAT: Criei o menu navegável do hub de eventos.

---
+++ nav.php
@@ -0,0 +1,6 @@
+<nav style="text-align: center">
+    <a href="index.php"><strong>Início</strong></a>
+    <a href="cadastro.php"><strong>Cadastro de Eventos</strong></a>
+    <a href="edicao.php"><strong>Edição de Eventos</strong></a>
+    <a href="remocao.php"><strong>Remoção de Eventos</strong></a>
+</nav>
